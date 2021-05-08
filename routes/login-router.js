a3b016c28a27b7720bcb98114915e4e909b4b3dc
Keshav <pritanikeshav52@gmail.com>
2021-05-09 01:05:03 +0530

    Login Code Working

--- routes/login-router.js
+++ routes/login-router.js
@@ -4,36 +4,40 @@ var router = express.Router();
 //data from database
 var visitorModel = require("../models/visitor-model");
 
-
+function validatePassword(visitor, login, res) {
+  if (visitor[0].password == login.password) {
+    res.status(200).json({ message: "Login Successfull" });
+  } else {
+    throw new Error("[ERROR] : Wrong Password");
+  }
+}
 
 router.post("/", async (req, res) => {
   const login = req.body;
   try {
     if (login.userrole == 0) {
-      let visitor = await visitorModel.find({ username : login.username });
-      // console.log(login);
-      // console.log(visitor);
-      if(!visitor)
-      {
-         visitor = await visitorModel.find({ contact : login.username });
-        if(!visitor)
-        {
-           visitor = await visitorModel.find({ email : login.username });
-            if(!visitor)
-            {
-              res.status(500).json({
-                message: error.message,
-              });
-            }
+      let visitor = await visitorModel.find({ username: login.username });
+      // console.log("UserName - " + visitor);
+      if (visitor.length == 0) {
+        visitor = await visitorModel.find({ email: login.username });
+        // console.log("Email - " + visitor);
+        if (visitor.length == 0) {
+          if (!isNaN(login.username))
+            visitor = await visitorModel.find({
+              contact: parseInt(login.username),
+            });
+          // console.log("Contact - " + visitor);
+          if (visitor.length == 0) {
+            throw new Error("[ERROR] : User Not found");
+          } else {
+            validatePassword(visitor, login, res);
+          }
+        } else {
+          validatePassword(visitor, login, res);
         }
-        else if (visitor[0].password == login.password) {
-          console.log("successfully Login....");    
-        }
+      } else {
+        validatePassword(visitor, login, res);
       }
-      else
-      res.status(500).json({
-        message: error.message,
-      });
     }
   } catch (error) {
     res.status(500).json({
