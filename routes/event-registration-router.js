632fbf489b975c69923b019e68e1df749a0b7b11
Mihir <76066631+202012078@users.noreply.github.com>
2021-05-09 21:34:43 +0530

    event registration form

---
+++ routes/event-registration-router.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+
+//
+router.get("/", async function (req, res) {
+    res.render("../views/pages/event-registration");
+});
+
+
+
+module.exports = router;
