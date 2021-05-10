5adbb3d2a7bc6aba7498c91f32db480f139e1942
Keshav <pritanikeshav52@gmail.com>
2021-05-10 16:51:38 +0530

    Store Table for loop added

--- routes/mall-owner/mall-owner-routes.js
+++ routes/mall-owner/mall-owner-routes.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var storeModel = require("../../models/store-model");
-
+var locationModel = require("../../models/location-model");
 var router = express.Router();
 
 router.get("/", async function (req, res) {
-    res.redirect("/mo/mo-dashboard");
-  });
+  res.redirect("/mo/mo-dashboard");
+});
 
 router.get("/mo-dashboard", async function (req, res) {
   res.render("../views/pages/mall-owner/dashboard");
@@ -13,14 +13,18 @@ router.get("/mo-dashboard", async function (req, res) {
 
 //get all stores
 router.get("/manage-stores", async function (req, res) {
-  console.log("[INFO] : Getting all stores");
   const all_stores = await storeModel.find({ is_deleted: false });
+
+  for (let i = 0; i < all_stores.length; i++) {
+    const locationId = await locationModel.findOne({
+      _id: all_stores[i].location_id,
+    });
+    all_stores[i].block = locationId.block_name;
+    all_stores[i].floor = locationId.floor_number;
+  }
   res.render("../views/pages/mall-owner/manage-stores", {
-    all_stores: all_stores
+    all_stores: all_stores,
   });
 });
-
-
-
 
 module.exports = router;
