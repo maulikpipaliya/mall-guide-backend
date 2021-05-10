13f56dbe5e200f97ef8c2470e2b24d1de5c05f84
Mihir <76066631+202012078@users.noreply.github.com>
2021-05-10 18:17:09 +0530

    add offer form

--- routes/store-owner/store-owner-routes.js
+++ routes/store-owner/store-owner-routes.js
@@ -14,4 +14,8 @@ router.get("/so-add-category", async function (req, res) {
   res.render("../views/pages/store-owner/add-catedory");
 });
 
+router.get("/so-add-offer", async function (req, res) {
+  res.render("../views/pages/store-owner/add-offer");
+});
+
 module.exports = router;
