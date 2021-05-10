1d0786717f34f01c2d36913e514bc84d9062af6b
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-05-11 05:08:35 +0530

    Configured UIs

--- assets/non-visitor/js/main-chart.js
+++ assets/non-visitor/js/main-chart.js
@@ -69,7 +69,7 @@ $(function() {
                 borderWidth: 1,
                 borderColor: "#eeeeee"
             },
-            colors: ["#65CEA7", "#424F63"],
+            colors: ["#65cea7", "#424F63"],
             tooltip: true,
             tooltipOpts: {
                 defaultTheme: false
