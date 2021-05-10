ed12918a0683c4e41f3f57336ffceb69dddd01f6
Mihir <76066631+202012078@users.noreply.github.com>
2021-05-10 17:51:55 +0530

    add category form

--- routes/store-owner/store-owner-routes.js
+++ routes/store-owner/store-owner-routes.js
@@ -10,5 +10,8 @@ router.get("/so-dashboard", async function (req, res) {
   res.render("../views/pages/store-owner/store-owner-dashboard");
 });
 
+router.get("/so-add-category", async function (req, res) {
+  res.render("../views/pages/store-owner/add-catedory");
+});
 
 module.exports = router;
