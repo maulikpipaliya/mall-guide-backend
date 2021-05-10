793d878a1d9abedb4edfae92b1f046133ae785fe
Keshav <pritanikeshav52@gmail.com>
2021-05-10 23:03:07 +0530

    Registration Successful

--- routes/visitor/registration-visitors-router.js
+++ routes/visitor/registration-visitors-router.js
@@ -10,7 +10,8 @@ router.post("/", async function (req, res) {
     const inserted = await newVisitor.save();
     if (!inserted) throw new Error("[ERROR] : Failed to insert");
     else console.log("[INFO] : Successfully Registered Visitor. ");
-    res.status(200).json(inserted);
+    // res.status(200).json(inserted);
+    res.redirect("/signin");
   } catch (error) {
     res.status(500).json({
       message: error.message,
