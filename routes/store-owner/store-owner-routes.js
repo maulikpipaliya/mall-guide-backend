cafc03e9f8449992d4beb5dbad7022b4d7496691
daya-yadav <60892973+daya-yadav@users.noreply.github.com>
2021-05-10 20:43:04 +0530

    Store-Request

--- routes/store-owner/store-owner-routes.js
+++ routes/store-owner/store-owner-routes.js
@@ -2,12 +2,16 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/", async function (req, res) {
+router.get("/", async function(req, res) {
     res.redirect("/so/so-dashboard");
-  });
+});
 
-router.get("/so-dashboard", async function (req, res) {
-  res.render("../views/pages/store-owner/store-owner-dashboard");
+router.get("/so-dashboard", async function(req, res) {
+    res.render("../views/pages/store-owner/store-owner-dashboard");
+});
+
+router.get("/so-store-request", async function(req, res) {
+    res.render("../views/pages/store-owner/store-request");
 });
 
 
